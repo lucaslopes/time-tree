a8a78cd9be41b4ee5a5f447162d7fdafbb53c9ee
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2024-04-02 06:46:36 +0200

    The default app is made with Flet instead of Streamlit.

--- app.py
+++ app.py
@@ -1,136 +1,183 @@
 import time
-import streamlit as st
-from datetime import datetime
+import tzlocal
+import threading
+import flet as ft
+from datetime import datetime, timezone
 from tinydb import TinyDB, Query
 
-# Initialize the database
-db_path = 'time_tree.json'
-db = TinyDB(db_path)
+class TimeTree:
+    def __init__(self, page: ft.Page, db_path: str = 'time_tree.json') -> None:
+        self.db = TinyDB(db_path)
+        self.page = page
+        self.main_attributes()
+        self.configure_page()
+        self.main_page()
+        self.update_entries_list()
+        self.update_current_task(self.root_task())
+    
+    def root_task(self):
+        return {'doc_id': 0, 'description': 'Root'}
 
-# Initialize or retrieve the current node
-if 'current_task' not in st.session_state:
-    st.session_state['current_task'] = 0
-if 'running' not in st.session_state:
-    st.session_state['running'] = False
-if 'start_time' not in st.session_state:
-    st.session_state['start_time'] = None
-if 'end_time' not in st.session_state:
-    st.session_state['end_time'] = None
+    def main_attributes(self):
+        self.running = False
+        self.start_time = None
+        self.end_time = None
+        self.description = None
+        self.current_task = 0
+        self.top_bar_title = ft.Text(value="")
+        self.path = ft.Text(value=self.get_path(), width=self.page.window_width * .95, height=self.page.window_height * .05, no_wrap=True, text_align=ft.alignment.center)
 
-def format_duration(duration):
-    # Helper function to format the duration into hh:mm:ss
-    return str(duration).split(".")[0]
+    def configure_page(self):
+        self.page.bgcolor = ft.colors.BROWN_600
+        self.page.padding = 0
+        self.page.window_width = 350
+        self.page.window_height = 450
+        self.page.window_resizable = True
+        self.page.window_always_on_top = True
+        self.page.title = "Time Tree"
 
-def update_stopwatch():
-    # Function to update and display the stopwatch time
-    if st.session_state['running']:
-        st.session_state['end_time'] = datetime.now()
-        duration = st.session_state['end_time'] - st.session_state['start_time']
-        st.session_state['stopwatch_display'].markdown(f"Stopwatch: {format_duration(duration)}")
+    def update_entries_list(self):
+        self.entries_list.content.controls.clear()
+        entries = self.db.search(Query().parent_id == self.current_task)
+        for entry in entries:
+            s_time, e_time = [entry[t][2:].replace('-', '/').replace('T', ' ') for t in ['start_time', 'end_time']]
+            list_tile = ft.Container(
+                ft.ListTile(
+                  title=ft.Text(value=f"{entry['description']}"),
+                  subtitle=ft.Text(value=f"Start: {s_time}\nEnd: {e_time}\nTime elapsed: {entry['duration']}s"),
+                  leading=ft.IconButton(icon=ft.icons.TASK, on_click=lambda e, entry=entry: self.update_current_task(entry, e))),
+                border = ft.border.all(1, ft.colors.WHITE),
+                border_radius = self.page.window_width * .025)
+            self.entries_list.content.controls.append(list_tile)
+        self.entries_list.update()
+        self.page.update()
 
-# Function to display the path
-def display_path(path_placeholder):
-    node = st.session_state['current_task']
-    path = str(node)
-    while node != 0:
-        node_data = db.search(Query().id == node)
-        if node_data and isinstance(node_data[0], dict) and 'parent_id' in node_data[0]:
-            parent = node_data[0]['parent_id']
-            node = parent
-            path = f"{parent} > {path}"
-        else:
-            break  # Break if the data structure is not as expected
-    path_placeholder.write(f"Path: {path}")
+    def update_current_task(self, entry = None, event = None):
+        entry = entry if entry else self.root_task()
+        self.current_task = entry['doc_id'] if 'doc_id' in entry else entry.doc_id
+        self.top_bar_title.value = self.format_duration()
+        self.path.value = self.get_path(entry)
+        self.update_entries_list()
+        self.page.update()
 
-# Function to display tasks from the database
-def display_tasks():
-    tasks = db.all()
-    if tasks:
-        # Adding the ID from the TinyDB as the first column
-        tasks_data = [[task.doc_id] + [task['start_time'], task['end_time'], task['duration'], task['description']] for task in tasks]
-        # Defining column names
-        columns = ['ID', 'Start Time', 'End Time', 'Duration', 'Description']
-        table_placeholder.table([columns] + tasks_data)
+    def format_duration(self):
+        return f'0{str(self.get_duration()).split(".")[0]}' if self.running else "00:00:00"
 
-# Function to move to parent node
-def go_back():
-    if st.session_state['current_task'] != 0:
-        parent = db.search(Query().id == st.session_state['current_task'])[0]['parent_id']
-        st.session_state['current_task'] = parent
-        st.experimental_rerun()
+    def update_time(self):
+        while self.running:
+            self.end_time = self.get_local_time()
+            self.top_bar_title.value = self.format_duration()
+            self.page.update()
+            time.sleep(1)
+        self.top_bar_title.value = self.format_duration()
 
-# Dynamic page title
-page_title = "Time Tree" if not st.session_state['running'] else format_duration(datetime.now() - st.session_state['start_time'])
-st.set_page_config(page_title=page_title, layout="centered")
+    def go_back(self, _):
+        if self.current_task != 0:
+            current_entry = self.db.get(doc_id = self.current_task)
+            parent_id = current_entry['parent_id']
+            parent_entry = self.db.get(doc_id = parent_id) if parent_id != 0 else None
+            self.update_current_task(parent_entry)
 
-# Create a placeholder for the button and text input
-path_placeholder = st.empty()
-back_placeholder = st.empty()
-button_placeholder = st.empty()
-description_placeholder = st.empty()
-table_placeholder = st.empty()
+    def get_identifier(self, entry):
+        return entry['description'] if len(entry['description']) > 0 else f'#{entry.doc_id}'
 
-# Display path
-display_path(path_placeholder)
+    def get_path(self, entry = None):
+        if self.current_task == 0:
+            return 'root'
+        # Build the path from the current task to the root
+        path = [self.get_identifier(entry)]
+        parent_id = entry['parent_id'] if 'parent_id' in entry else 0
+        while parent_id != 0:
+            parent_entry = self.db.get(doc_id=parent_id)
+            path.insert(0, self.get_identifier(parent_entry))
+            parent_id = parent_entry.get('parent_id', 0)
+        path.insert(0, 'root')
+        return " > ".join(path)
 
-# Back button
-if back_placeholder.button('Back'):
-    go_back()
+    def get_local_time(self):
+        return datetime.now(timezone.utc).astimezone(tzlocal.get_localzone())
+    
+    def get_duration(self):
+        return self.end_time - self.start_time
 
-# Create a button in the placeholder to start/stop the stopwatch with unique keys
-if not st.session_state['running']:
-    main_button = button_placeholder.button('Start', key='start_button')
-else:
-    main_button = button_placeholder.button('Stop', key='stop_button')
-task_description = ''
+    def add_entry(self):
+        self.db.insert({
+            'parent_id': self.current_task,
+            'start_time': self.start_time.isoformat(timespec='seconds'), 
+            'end_time': self.end_time.isoformat(timespec='seconds'), 
+            'duration': round(self.get_duration().total_seconds()), 
+            'description': self.description})
 
-if main_button:
-    if not st.session_state['running']:
-        # Start the stopwatch
-        st.session_state['running'] = True
-        st.session_state['start_time'] = datetime.now()
-        st.session_state['stopwatch_display'] = st.empty()
-        table_placeholder = st.empty()
-    else:
-        # Stop the stopwatch
-        st.session_state['running'] = False
-        end_time = datetime.now()
-        duration = end_time - st.session_state['start_time']
-        # Save to TinyDB including the task description
-        new_node_id = len(db) + 1
-        db.insert({
-            'id': new_node_id,
-            'parent_id': st.session_state['current_task'],
-            'start_time': st.session_state['start_time'].strftime('%Y-%m-%d %H:%M:%S'), 
-            'end_time': end_time.strftime('%Y-%m-%d %H:%M:%S'), 
-            'duration': format_duration(duration),
-            'description': st.session_state['task_description']
-        })
-        st.session_state['stopwatch_display'].markdown(f"Stopwatch stopped at: {format_duration(duration)}")
-        description_placeholder.empty()
-        button_placeholder.empty()
-        # Ensure the button switches back to "Start"
-        main_button = button_placeholder.button('Start', key='restart_button')
-        task_description = ''
-        st.experimental_rerun()
+    def stopper(self):
+        self.running = False
+        self.end_time = self.get_local_time()
+        self.description = self.description_input.content.value
+        self.add_entry()
+        self.description_input.content.value = ""
+        self.description_input.visible = False
+        self.entries_list.visible = True
+        self.top_bar.content.controls[0].visible = True
+        self.top_bar.content.controls[1].visible = True
+        self.top_bar.content.controls[-1].visible = True
+        self.top_bar.content.controls[-2].visible = True
+        self.main_button.icon = "play_arrow"
+        self.main_button.tooltip = "Start Timer"
+        self.main_button.bgcolor = ft.colors.GREEN_700
+        self.top_bar.bgcolor = ft.colors.BROWN_800
+        self.page.bgcolor = ft.colors.BROWN_600
+        self.update_entries_list()
+        self.update_time()
+    
+    def starter(self):
+        self.running = True
+        self.start_time = self.get_local_time()
+        self.end_time = None
+        self.description_input.visible = True
+        self.entries_list.visible = False
+        self.top_bar.content.controls[0].visible = False
+        self.top_bar.content.controls[1].visible = False
+        self.top_bar.content.controls[-1].visible = False
+        self.top_bar.content.controls[-2].visible = False
+        self.main_button.icon = "stop"
+        self.main_button.tooltip = "Stop Timer"
+        self.main_button.bgcolor = ft.colors.BROWN_700
+        self.top_bar.bgcolor = ft.colors.GREEN_800
+        self.page.bgcolor = ft.colors.GREEN_600
+        threading.Thread(target=self.update_time).start()
+    
+    def toggle_timer(self, _):
+        self.stopper() if self.running else self.starter()
+        self.page.update()
+    
+    def main_page(self):
+        home_button = ft.IconButton(icon=ft.icons.HOME, on_click=lambda e: self.update_current_task(), icon_color=ft.colors.WHITE)
+        back_button = ft.IconButton(icon=ft.icons.ARROW_BACK, on_click=self.go_back, icon_color=ft.colors.WHITE)
+        edit_button = ft.IconButton(icon=ft.icons.EDIT, on_click=lambda e: True, icon_color=ft.colors.WHITE)
+        settings_button = ft.IconButton(icon=ft.icons.SETTINGS, on_click=lambda e: True, icon_color=ft.colors.WHITE)
+        self.top_bar = ft.Container(
+          content=ft.Row(controls=[home_button, back_button, self.top_bar_title, edit_button, settings_button]),  # Add back button to top bar
+          bgcolor=ft.colors.BROWN_800,
+          width=self.page.window_width,
+          height=self.page.window_height * .1)
+        self.description_input = ft.Container(
+            ft.TextField(hint_text="Enter task description", expand=True, width=self.page.window_width * .95, height=self.page.window_height * .1),
+            alignment=ft.alignment.center,
+            visible=False)
+        self.entries_list = ft.Container(
+            ft.ListView(
+              width=self.page.window_width * .95,
+              height=self.page.window_height * .8,
+              spacing=self.page.window_width * .025),
+            alignment=ft.alignment.top_center,
+            margin=0,
+            padding=0,
+          )
+        self.main_button = ft.FloatingActionButton(icon="play_arrow", tooltip="Start Timer", on_click=self.toggle_timer, bgcolor=ft.colors.GREEN_700)
+        self.page.add(self.main_button)
+        self.page.add(self.top_bar)
+        self.page.add(self.path)
+        self.page.add(self.description_input)
+        self.page.add(self.entries_list)
+        self.update_entries_list()
 
-# Update the stopwatch every second if it's running
-if st.session_state['running']:
-    while st.session_state['running']:
-        if task_description == '':
-          task_description = description_placeholder.text_input("Task Description", key="task_description")
-        update_stopwatch()
-        time.sleep(1)
-        # Update page title in real-time
-        st.experimental_rerun()
-    st.session_state['task_description'] = task_description
-else:
-    # Display child nodes and navigation
-    current_task = st.session_state['current_task']
-    children = db.search(Query().parent_id == int(current_task))
-    if children and all(isinstance(child, dict) and 'id' in child for child in children):
-        for child in children:
-            if child['id'] != current_task:
-              if st.button(f"Go to node {child['id']}"):
-                  st.session_state['current_task'] = child['id']
-                  st.experimental_rerun()
+ft.app(target=TimeTree)
