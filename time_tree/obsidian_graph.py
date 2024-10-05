127d73ca386a45686a4473990b17864e8ea95c33
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2024-10-05 00:53:50 -0300

    Enhance Obsidian graph visualization with Streamlit integration and layout adjustments

--- time_tree/obsidian_graph.py
+++ time_tree/obsidian_graph.py
@@ -1,13 +1,32 @@
+import networkx as nx
+import streamlit as st
+import streamlit.components.v1 as components
 import obsidiantools.api as otools
 from IPython.display import display, HTML
 from pyvis.network import Network
+import io
 
 VAULT_DIRECTORY = '/Users/lucas/Documents/TimeTree'
 vault = otools.Vault(VAULT_DIRECTORY).connect().gather()
-
 G = vault.graph
-net = Network(notebook=False)
+
+net = Network(
+  notebook=False,
+  height='400px',
+  width='100%',
+  bgcolor='#222222',
+  font_color='white')
 net.from_nx(G)
 
-net.save_graph("networkx-pyvis.html")
-HTML(filename="networkx-pyvis.html")
+# Generate network with specific layout settings
+net.repulsion(
+  node_distance=420,
+  central_gravity=0.33,
+  spring_length=110,
+  spring_strength=0.10,
+  damping=0.95)
+
+st.title('Obsidian Graph')
+
+# Load HTML content in the Streamlit component
+components.html(net.generate_html(), height=435)
