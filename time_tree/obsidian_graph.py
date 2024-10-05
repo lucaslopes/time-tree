667fcc63bd8975f6528d3530d0fc1d6f37f4cd08
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2024-10-05 03:13:24 -0300

    Filter nodes that are in TimeTree folder

--- time_tree/obsidian_graph.py
+++ time_tree/obsidian_graph.py
@@ -1,12 +1,15 @@
 import networkx as nx
 import streamlit as st
-import streamlit.components.v1 as components
-import obsidiantools.api as otools
+from pathlib import Path
 from pyvis.network import Network
+from obsidiantools import api as otools
+from streamlit.components import v1 as components
 
-VAULT_DIRECTORY = '/Users/lucas/Documents/TimeTree'
+VAULT_DIRECTORY = Path('/Users/lucas/Documents/TimeTree')
 vault = otools.Vault(VAULT_DIRECTORY).connect().gather()
-G = vault.graph
+df = vault.get_note_metadata()
+nodes_list = df[df['rel_filepath'].astype(str).str.startswith('TimeTree/')].index.tolist()
+G = vault.graph.subgraph(nodes_list).copy()
 
 net = Network(
   notebook=False,
@@ -14,7 +17,6 @@ net = Network(
   width='100%',
   bgcolor='#222222',
   font_color='white')
-net.from_nx(G)
 
 # Generate network with specific layout settings
 net.repulsion(
@@ -23,6 +25,10 @@ net.repulsion(
   spring_length=110,
   spring_strength=0.10,
   damping=0.95)
+
+net.from_nx(G)
+for e in net.edges:
+  e['width'] = 1.5
 
 st.title('Obsidian Graph')
 
